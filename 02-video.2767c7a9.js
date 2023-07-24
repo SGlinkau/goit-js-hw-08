219acee1831aa7ba26b5f7b5ede19ecfaf590e65
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-07-24 16:55:19 +0000

    Deploying to gh-pages from @ SGlinkau/goit-js-hw-08@d96a7624cb4372e1f6f08eed112f5e61aa4b4dd1 🚀

---
+++ 02-video.2767c7a9.js
@@ -0,0 +1,2 @@
+player.getCurrentTime().then((function(n){})).catch((function(n){}));
+//# sourceMappingURL=02-video.2767c7a9.js.map
---
+++ 02-video.3b3a9bc6.js
@@ -0,0 +1,2 @@
+player.getCurrentTime().then((function(n){})).catch((function(n){}));
+//# sourceMappingURL=02-video.3b3a9bc6.js.map
