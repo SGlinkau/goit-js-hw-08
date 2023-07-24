d96a7624cb4372e1f6f08eed112f5e61aa4b4dd1
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-07-24 18:54:51 +0200

    Update
    "Zrobiono" (chyba) zad. 1 i próba ogarnięcia 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "../../node_modules/simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const img = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+img.insertAdjacentHTML("beforeend", markup);
+
+img.addEventListener("click", lightBoxModal);
+
+function lightBoxModal(e) {
+  e.preventDefault();
+  const target = e.target;
+  const image = target.dataset.source;
+  if (e.target.nodeName !== "a.gallery__link") {
+    const modal = basicLightbox.create(
+      `
+        <img width="1400" height="900" src="${image}">
+	`
+    );
+    modal.show();
+
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") modal.close();
+    });
+  }
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,5 @@
-
+player.getCurrentTime().then(function(seconds) {
+    // seconds = the current playback position
+}).catch(function(error) {
+    // an error occurred
+});
