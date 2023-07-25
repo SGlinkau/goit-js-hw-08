85c38d8c800d2125a94c0a7c73c4b83584f75eb5
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-07-25 17:41:43 +0200

    Updates
    Updated task 2 - 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,15 @@
-player.getCurrentTime().then(function(seconds) {
-    // seconds = the current playback position
-}).catch(function(error) {
-    // an error occurred
-});
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const video = document.querySelector("#vimeo-player");
+
+const player = new Player(video);
+
+player.on(
+  "timeupdate",
+  throttle(function playTime(e) {
+    localStorage.setItem("vid-time", e.seconds)
+  }, 1000),
+);
+
+player.setCurrentTime(localStorage.getItem('vid-time'));
